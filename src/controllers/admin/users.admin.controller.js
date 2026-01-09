557d8d6ec25ff273b88c4e957d6b10825d50b0d9
Bhavin Panara <panarabk@gmail.com>
2026-01-09 18:22:18 +0530

    Add missing controller in user-admin controller

--- src/controllers/admin/users.admin.controller.js
+++ src/controllers/admin/users.admin.controller.js
@@ -7,6 +7,24 @@ const {
     setRolesSchema,
 } = require("../../validations/admin/users.admin.validation");
 
+async function list(req, res) {
+    const query = listUsersQuerySchema.parse(req.query || {});
+
+    const data = await AdminUsersService.listUsers({
+        query,
+    });
+
+    return ResponseUtil.ok(res, 200, data);
+}
+
+async function getById(req, res) {
+    const data = await AdminUsersService.getUserById({
+        userId: req.params.id,
+    });
+
+    return ResponseUtil.ok(res, 200, data);
+}
+
 async function create(req, res) {
     const body = createAdminUserSchema.parse(req.body);
 
@@ -31,4 +49,6 @@ async function setRoles(req, res) {
 module.exports = {
     create,
     setRoles,
+    list,
+    getById
 };
